f7b59cddd41ba9c406ee978c51ffa94f012d6398
NAPOLI, Lachlan <lachlanjnapoli@gmail.com>
2024-11-15 16:58:56 +1100

    Add ReadFiles to FileReader

--- PortfolioOverlap/Classes/FileReader.cs
+++ PortfolioOverlap/Classes/FileReader.cs
@@ -2,8 +2,11 @@ namespace PortfolioOverlap.Classes
 {
     public static class FileReader
     {
-        public static string ReadFile(string FileName) {
-            return "";
+        public static string ReadFile(string fileName) {
+            string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName ?? throw new NullReferenceException();
+            string filePath = Path.Combine(projectDirectory, fileName);
+            string text = File.ReadAllText(filePath);
+            return text;
         }
     }
 }
